fa7bd2a0d5ad2b0c692c621f92718691339df3fe
Morten Eriksen <mortene@sim.no>
2002-01-09 11:57:53 +0000

    Moves private struct definition from viewers/SoGtkFullViewerP.h.

---
+++ src/Inventor/Gtk/widgets/SoGtkViewerButtonP.h
@@ -0,0 +1,34 @@
+/**************************************************************************
+ *
+ *  This file is part of the Coin SoGtk GUI binding library.
+ *  Copyright (C) 2000 by Systems in Motion.  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  version 2.1 as published by the Free Software Foundation.  See the
+ *  file LICENSE.LGPL at the root directory of the distribution for
+ *  more details.
+ *
+ *  If you want to use Coin SoGtk for applications not compatible with the
+ *  LGPL, please contact SIM to acquire a Professional Edition License.
+ *
+ *  Systems in Motion, Prof Brochs gate 6, N-7030 Trondheim, NORWAY
+ *  http://www.sim.no/ support@sim.no Voice: +47 22114160 Fax: +47 22207097
+ *
+ **************************************************************************/
+
+// $Id$
+
+#ifndef SOGTK_VIEWERBUTTONP_H
+#define SOGTK_VIEWERBUTTONP_H
+
+#include <gtk/gtk.h>
+
+struct SoGtkViewerButton {
+  char * keyword;
+  char * label;
+  GtkSignalFunc pressed;
+  char ** xpm_data;
+};
+
+#endif // ! SOGTK_VIEWERBUTTONP_H
