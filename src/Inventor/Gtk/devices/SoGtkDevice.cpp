3497881a342dabe1c3201c05a48fd9b057294a99
Morten Eriksen <mortene@sim.no>
2002-06-25 12:32:28 +0000

    Reorganize to share lots of implementation code for the So@Gui@Device class.

--- src/Inventor/Gtk/devices/SoGtkDevice.cpp
+++ src/Inventor/Gtk/devices/SoGtkDevice.cpp
@@ -19,27 +19,11 @@
 
 #if HAVE_CONFIG_H
 #include <config.h>
-#endif
-
-#include <Inventor/errors/SoDebugError.h>
-#include <Inventor/events/SoEvent.h>
+#endif // HAVE_CONFIG_H
 
 #include <sogtkdefs.h>
 #include <Inventor/Gtk/devices/SoGtkDevice.h>
-#include <Inventor/Gtk/devices/SoGtkInputFocus.h>
-#include <Inventor/Gtk/devices/SoGtkKeyboard.h>
-#include <Inventor/Gtk/devices/SoGtkMouse.h>
-#include <Inventor/Gtk/devices/SoGtkSpaceball.h>
-
-// *************************************************************************
-
-/*!
-  \class SoGtkDevice SoGtkDevice.h Inventor/Gtk/devices/SoGtkDevice.h
-  \brief The SoGtkDevice class ...
-  \ingroup devices
-
-  FIXME: write class doc
-*/
+#include <Inventor/Gtk/devices/SoGtkDeviceP.h>
 
 // *************************************************************************
 
@@ -49,45 +33,11 @@
   FIXME: write doc
 */
 
-/*!
-  \fn void SoGtkDevice::enable(GtkWidget * widget, SoGtkEventHandler * func, void * closure) = 0
-
-  FIXME: write doc
-*/
-
-/*!
-  \fn void SoGtkDevice::disable(GtkWidget * widget, SoGtkEventHandler * func, void * closure) = 0
-
-  FIXME: write doc
-*/
-
-/*!
-  \fn const SoEvent * SoGtkDevice::translateEvent(GdkEvent * event)
-
-  FIXME: write doc
-*/
-
-// *************************************************************************
-
-class SoGtkDeviceP {
-public:
-  static SbVec2s lastEventPosition;
-  SbVec2s widgetSize;
-};
-
-SbVec2s SoGtkDeviceP::lastEventPosition; // static
-
-#define PRIVATE(p) (p->pimpl)
-
-// *************************************************************************
-
-SOGTK_OBJECT_ABSTRACT_SOURCE(SoGtkDevice);
-
 // *************************************************************************
 
 SoGtkDevice::SoGtkDevice(void)
 {
-  PRIVATE(this) = new SoGtkDeviceP;
+  PRIVATE(this) = new SoGtkDeviceP(this);
 }
 
 SoGtkDevice::~SoGtkDevice()
@@ -95,92 +45,18 @@ SoGtkDevice::~SoGtkDevice()
   delete PRIVATE(this);
 }
 
-/*!
-  This function initializes the type system for all the device classes.
-*/
-
-void
-SoGtkDevice::initClasses(void)
-{
-  SoGtkDevice::initClass();
-  SoGtkInputFocus::initClass();
-  SoGtkMouse::initClass();
-  SoGtkKeyboard::initClass();
-  SoGtkSpaceball::initClass();
-}
-
-// *************************************************************************
-
-/*!
-  FIXME: write function documentation
-*/
-
-// FIXME: perhaps make virtual?
-void
-SoGtkDevice::setWindowSize(const SbVec2s size)
-{
-  PRIVATE(this)->widgetSize = size;
-}
-
-/*!
-  FIXME: write function documentation
-*/
-
-SbVec2s
-SoGtkDevice::getWindowSize(void) const
-{
-  return PRIVATE(this)->widgetSize;
-}
-
-// *************************************************************************
-
-/*!
-  FIXME: write function documentation
-*/
-
-void
-SoGtkDevice::setEventPosition(SoEvent * event,
-                              int x,
-                              int y) const
-{
-  SoGtkDeviceP::lastEventPosition = SbVec2s(x, y);
-  event->setPosition(SbVec2s(x, PRIVATE(this)->widgetSize[1] - y - 1));
-}
-
-// *************************************************************************
-
-/*!
-  FIXME: write function documentation
-*/
-
-SbVec2s
-SoGtkDevice::getLastEventPosition(void)
-{
-  return SoGtkDeviceP::lastEventPosition;
-}
-
-// *************************************************************************
-
-void
-SoGtkDevice::addEventHandler(GtkWidget *, SoGtkEventHandler *, void *)
-{
-  // FIXME: not used here, but in other toolkits..?
-  // Investigate. 20020624 mortene.
-  SOGTK_STUB();
-}
-
-void
-SoGtkDevice::removeEventHandler(GtkWidget *, SoGtkEventHandler *, void *)
-{
-  // FIXME: not used here, but in other toolkits..?
-  // Investigate. 20020624 mortene.
-  SOGTK_STUB();
-}
-
 void
 SoGtkDevice::invokeHandlers(GdkEvent * event)
 {
-  // FIXME: not used here, but in other toolkits..?
-  // Investigate. 20020624 mortene.
-  SOGTK_STUB();
+  PRIVATE(this)->invokeHandlers(SoGtkDeviceP::invokeHandlerCB, event);
+}
+
+SbBool
+SoGtkDeviceP::invokeHandlerCB(SoGtkEventHandler * handler,
+                              GtkWidget * widget,
+                              GdkEvent * event,
+                              void * handlerclosure)
+{
+  (void)handler(widget, event, handlerclosure);
+  return TRUE;
 }
