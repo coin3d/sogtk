70a87707d8566e1775f8081c9c2d0519d2d41431
Morten Eriksen <mortene@sim.no>
2002-06-25 19:44:16 +0000

    Updated to compile with Cheshire Cat pattern.

--- src/Inventor/Gtk/devices/SoGtkInputFocus.cpp
+++ src/Inventor/Gtk/devices/SoGtkInputFocus.cpp
@@ -23,118 +23,53 @@
 
 #include <sogtkdefs.h>
 #include <Inventor/Gtk/devices/SoGtkInputFocus.h>
+#include <Inventor/Gtk/devices/SoGuiInputFocusP.h>
 
 // *************************************************************************
 
-/*!
-  \class SoGtkInputFocus SoGtkInputFocus.h Inventor/Gtk/devices/SoGtkInputFocus.h
-  \brief The SoGtkInputFocus class is a device for handling focus events.
-  \ingroup devices
-
-  The SoGtkInputFocus class does not translate events like the other
-  devices, it is only added for snooping events for the components.
-
-  This class has no functionality yet...
-*/
+class SoGtkInputFocusP : public SoGuiInputFocusP {
+public:
+  SoGtkInputFocusP(SoGtkInputFocus * p) : SoGuiInputFocusP(p) { }
+};
 
 // *************************************************************************
 
-/*!
-  \enum SoGtkInputFocus::Events
-
-  Bit-wise enumeration over events handled by the SoGtkInputFocus device class.
-  Used in the SoGtkInputFocus constructor.
-*/
-
-/*!
-  \var SoGtkInputFocus::Events SoGtkInputFocus::ENTER_WINDOW
-
-  The event emitted when focus comes to a widget.
-*/
-
-/*!
-  \var SoGtkInputFocus::Events SoGtkInputFocus::LEAVE_WINDOW
-
-  The event emitted when focus leaves a widget.
-*/
-
-/*!
-  \var SoGtkInputFocus::Events SoGtkInputFocus::ALL_EVENTS
-
-  All the events handled by the SoGtkInputFocus device.
-*/
-
-// *************************************************************************
-// The type system.
-
-SOGTK_OBJECT_SOURCE(SoGtkInputFocus);
-
-// *************************************************************************
-
-/*!
-  Constructor.
-
-  The \a eventbits argument decides which events are handled.
-  It defaults to SoGtkInputFocus::ALL_EVENTS.
-*/
-
-SoGtkInputFocus::SoGtkInputFocus(
-  const int eventbits)
+SoGtkInputFocus::SoGtkInputFocus(const int eventbits)
 {
-  this->eventmask = eventbits;
-} // SoGtkInputFocus()
-
-/*!
-  Destructor.
-*/
+  PRIVATE(this) = new SoGtkInputFocusP(this);
+  PRIVATE(this)->eventmask = eventbits;
+}
 
 SoGtkInputFocus::~SoGtkInputFocus()
 {
-} // ~SoGtkInputFocus()
+  delete PRIVATE(this);
+}
 
 // *************************************************************************
 
-/*!
-  FIXME: write function documentation
-*/
-
 void
-SoGtkInputFocus::enable(
-  GtkWidget * widget,
-  SoGtkEventHandler * func,
-  void * data)
+SoGtkInputFocus::enable(GtkWidget * widget,
+                        SoGtkEventHandler * func,
+                        void * data)
 {
   SOGTK_STUB();
-} // enable()
-
-/*!
-  FIXME: write function documentation
-*/
+}
 
 void
-SoGtkInputFocus::disable(
-  GtkWidget * widget,
-  SoGtkEventHandler * func,
-  void * data)
+SoGtkInputFocus::disable(GtkWidget * widget,
+                         SoGtkEventHandler * func,
+                         void * data)
 {
   SOGTK_STUB();
-} // disable()
+}
 
 // *************************************************************************
-
-/*!
-  This method snoops on the events, and deals with the input focus related
-  events.
-
-  No functionality has been implemented yet.
-*/
 
 const SoEvent *
-SoGtkInputFocus::translateEvent(
-  GdkEvent * event)
+SoGtkInputFocus::translateEvent(GdkEvent * event)
 {
   SOGTK_STUB();
   return NULL;
-} // translateEvent()
+}
 
 // *************************************************************************
